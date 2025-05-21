10c4e67fe703a5daa89d788b89fa8e3a41d8cc54
artemqua3344 <artem.nek12@yandex.ru>
2025-05-21 17:56:34 +0400

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function getJsonValues(filename="data.json") {
+async function getJsonValues(filename = "data.json") {
     try {
         const response = await fetch(filename);
         if (!response.ok) {
@@ -9,8 +9,27 @@ async function getJsonValues(filename="data.json") {
     } catch (error) {
         console.error("Error fetching JSON data:", error);
     }
-    
+
 }
 
 let jsonData = getJsonValues();
+let HTML = ``;
+
+function buildTable(jsonData) {
+
+    for (let i = 0; i <= jsonData.length; i++) {
+        const name = jsonData[i].name;
+        const comment = jsonData[i].comment;
+
+        HTML += `
+        <div class="comment-div">
+            <div class="title">${name}</div>
+            <div class="comment">${comment}</div>
+        </div>`;
+
+    }
+    document.getElementById("output").innerHTML = buildTable(jsonData);
+}
+
+
 console.log(jsonData);
